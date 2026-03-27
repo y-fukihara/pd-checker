35dac0888fb995eacfa54f88efed39bcb5d5fb4e
y.fukihara <yf.vermilion@gmail.com>
2026-03-27 17:02:16 +0900

    router implement

--- src/App.tsx
+++ src/App.tsx
@@ -142,99 +142,109 @@ function App() {
 
   return (
     <div id="app">
-      <form id="form-name" name="search-by-name" accessKey="q">
-        <fieldset id="field-region">
-          <legend>適用ルール</legend>
-          <div className="field-item">
-            <label htmlFor="region-50yrs">50年:</label>
-            <input type="radio" name="region" id="region-50yrs" onChange={() => setConfigRegion("50yrs")} />
-            <FontAwesomeIcon icon={["far", "circle-question"]} />
-          </div>
-          <div className="field-item">
-            <label htmlFor="region-75yrs">75年:</label>
-            <input type="radio" defaultChecked name="region" value="75yrs" id="region-75yrs" onChange={() => setConfigRegion("75yrs")} />
-            <FontAwesomeIcon icon={["far", "circle-question"]} />
-          </div>
-          <div className="field-item">
-            <label htmlFor="region-jpn">日本 (未実装):</label>
-            <input type="radio" disabled name="region" value="jpn" id="region-jpn" />
-            <FontAwesomeIcon icon={["far", "circle-question"]} />
-          </div>
-        </fieldset>
-        <label htmlFor="name">作曲家の名前:</label>
-        <input type="text" id="name" name="name" onChange={(e) => {setQueryName(e.target.value)}}></input>
-      </form>
-      <div id="result">
-        <h2>検索結果</h2>
-        <p>検索クエリ: <span id="show-query">{query_name}</span></p>
-        <div id="result-list">
-          {composersResultList
-            .map((person) => (
-              <div key={`result-${person.id}`} className="result-item">
-                <div className="result-heading">
-                  <span className="familyname">{person.name[0].last},</span> {person.name[0].given}
-                </div>
-                <div className="result-details">
-                  <div className="summary">
-                    <span className={`result-summary-${person.summary}`}>{summaryString(person.summary)}</span>
-                  </div>
-                  <div className="expiration">
-                    <div className="label">保護期間終了日</div>
-                    <div className="date">{person.expiration.year}-{person.expiration.month.toString().padStart(2,'0')}-{person.expiration.day.toString().padStart(2,'0')}</div>
-                  </div>
-                  <div className="lifetime">
-                    <div className="birth">
-                      <div className="label">出生日</div>
-                      <div className="date">{person.birth.year}-{person.birth.month.toString().padStart(2, '0')}-{person.birth.day.toString().padStart(2, '0')}</div>
-                    </div>
-                    <div className="death">
-                      <div className="label">死亡日</div>
-                      <div className="date">{person.death.year}-{person.death.month.toString().padStart(2, '0')}-{person.death.day.toString().padStart(2, '0')}</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          }
-        </div>
+      <hgroup>
+        <h1>作曲家PDチェッカー</h1>
+        <p>作曲家の生没年と国籍から、著作権の存続期間を判定します。</p>
+      </hgroup>
+      <div id="disclaimer">
+        <h2>免責事項</h2>
+        <p>本ツールは法的なアドバイスを提供するものではありません。あくまで参考情報としてご利用ください。</p>
       </div>
-      <div id="composers-list">
-        <div className="wrapper">
-          <h2>Composers List</h2>
-          <table>
-            <colgroup>
-              <col className="composer-id" />
-              <col className="composer-name" />
-              <col className="composer-birth" />
-              <col className="composer-death" />
-            </colgroup>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Birth</th>
-                <th>Death</th>
-              </tr>
-            </thead>
-            <tbody>
-              {composersList.map((person) => (
-                <tr key={`list-${person.id}`}>
-                  <td>{person.id}</td>
-                  <td>
-                    {person.name[0].last}, {person.name[0].given}
-                  </td>
-                  <td>
-                    {person.birth.year}-{person.birth.month.toString().padStart(2, '0')}-{person.birth.day.toString().padStart(2, '0')}
-                  </td>
-                  <td>
-                    {person.death.year}-{person.death.month.toString().padStart(2, '0')}-{person.death.day.toString().padStart(2, '0')}
-                  </td>
+      <main>
+        <form id="form-name" name="search-by-name" accessKey="q">
+          <fieldset id="field-region">
+            <legend>適用ルール</legend>
+            <div className="field-item">
+              <label htmlFor="region-50yrs">50年:</label>
+              <input type="radio" name="region" id="region-50yrs" onChange={() => setConfigRegion("50yrs")} />
+              <FontAwesomeIcon icon={["far", "circle-question"]} />
+            </div>
+            <div className="field-item">
+              <label htmlFor="region-75yrs">75年:</label>
+              <input type="radio" defaultChecked name="region" value="75yrs" id="region-75yrs" onChange={() => setConfigRegion("75yrs")} />
+              <FontAwesomeIcon icon={["far", "circle-question"]} />
+            </div>
+            <div className="field-item">
+              <label htmlFor="region-jpn">日本 (未実装):</label>
+              <input type="radio" disabled name="region" value="jpn" id="region-jpn" />
+              <FontAwesomeIcon icon={["far", "circle-question"]} />
+            </div>
+          </fieldset>
+          <label htmlFor="name">作曲家の名前:</label>
+          <input type="text" id="name" name="name" onChange={(e) => {setQueryName(e.target.value)}}></input>
+        </form>
+        <div id="result">
+          <h2>検索結果</h2>
+          <p>検索クエリ: <span id="show-query">{query_name}</span></p>
+          <div id="result-list">
+            {composersResultList
+              .map((person) => (
+                <div key={`result-${person.id}`} className="result-item">
+                  <div className="result-heading">
+                    <span className="familyname">{person.name[0].last},</span> {person.name[0].given}
+                  </div>
+                  <div className="result-details">
+                    <div className="summary">
+                      <span className={`result-summary-${person.summary}`}>{summaryString(person.summary)}</span>
+                    </div>
+                    <div className="expiration">
+                      <div className="label">保護期間終了日</div>
+                      <div className="date">{person.expiration.year}-{person.expiration.month.toString().padStart(2,'0')}-{person.expiration.day.toString().padStart(2,'0')}</div>
+                    </div>
+                    <div className="lifetime">
+                      <div className="birth">
+                        <div className="label">出生日</div>
+                        <div className="date">{person.birth.year}-{person.birth.month.toString().padStart(2, '0')}-{person.birth.day.toString().padStart(2, '0')}</div>
+                      </div>
+                      <div className="death">
+                        <div className="label">死亡日</div>
+                        <div className="date">{person.death.year}-{person.death.month.toString().padStart(2, '0')}-{person.death.day.toString().padStart(2, '0')}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div id="composers-list">
+          <div className="wrapper">
+            <h2>Composers List</h2>
+            <table>
+              <colgroup>
+                <col className="composer-id" />
+                <col className="composer-name" />
+                <col className="composer-birth" />
+                <col className="composer-death" />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Birth</th>
+                  <th>Death</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {composersList.map((person) => (
+                  <tr key={`list-${person.id}`}>
+                    <td>{person.id}</td>
+                    <td>
+                      {person.name[0].last}, {person.name[0].given}
+                    </td>
+                    <td>
+                      {person.birth.year}-{person.birth.month.toString().padStart(2, '0')}-{person.birth.day.toString().padStart(2, '0')}
+                    </td>
+                    <td>
+                      {person.death.year}-{person.death.month.toString().padStart(2, '0')}-{person.death.day.toString().padStart(2, '0')}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 /* Sanitization CSS */
 import 'sanitize.css'
 import 'sanitize.css/forms.css'
@@ -9,7 +10,14 @@ import 'sanitize.css/system-ui.css'
 /* Workspace and component CSS */
 import './index.css'
 import './main.css'
+/* Sub-component */
 import App from './App.tsx'
+import AboutProduct from './AboutProduct.tsx'
+
+const router = createBrowserRouter([
+  { path: '/', Component: App },
+  { path: "/about", Component: AboutProduct },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -19,16 +27,6 @@ createRoot(document.getElementById('root')!).render(
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
     <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
 
-    <hgroup>
-      <h1>作曲家PDチェッカー</h1>
-      <p>作曲家の生没年と国籍から、著作権の存続期間を判定します。</p>
-    </hgroup>
-    <div id="disclaimer">
-      <h2>免責事項</h2>
-      <p>本ツールは法的なアドバイスを提供するものではありません。あくまで参考情報としてご利用ください。</p>
-    </div>
-    <main>
-      <App />
-    </main>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
