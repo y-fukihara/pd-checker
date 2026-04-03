31383ec36c3ebf31e8a823531da4710b9022ae83
y.fukihara <yf.vermilion@gmail.com>
2026-04-03 20:38:37 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
     Component: Header,
     children: [
       { index: true, Component: App },
-      { path: "/about", Component: AboutProduct },
+      { path: "about", Component: AboutProduct },
     ]
   },
 ], {
